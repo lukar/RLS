88f532f8229006e76306f70b1fa80df7843d0b0e
lukar <lukar.git@gmail.com>
2021-05-25 19:59:32 +0200

    Operator overloads and simplifications

--- RecursiveLeastSquares/RecursiveLeastSquares/rls.h
+++ RecursiveLeastSquares/RecursiveLeastSquares/rls.h
@@ -2,21 +2,102 @@
 #include <iostream>
 #include <array> 
 
-template <typename T,size_t Row, size_t Col> using Matrix = std::array<std::array<T, Col>, Row>;
+template <size_t N> using Matrix = std::array<std::array<double, N>, N>;
+
+template <int N>
+std::array<double, N> operator*(double c, const std::array<double, N>& A)
+{
+    std::array<double, N> retv{};
+    for (int i = 0; i < N; i++) {
+        retv[i] = c * A[i];
+    }
+    return retv;
+}
+
+template <int N>
+std::array<double, N> operator*( const std::array<double, N>& A,double c)
+{
+    std::array<double, N> retv{};
+    for (int i = 0; i < N; i++) {
+        retv[i] = c * A[i];
+    }
+    return retv;
+}
+
+template <int N>
+Matrix<N> operator*(const Matrix<N> &L, const Matrix<N> &R)
+{
+    Matrix<N> retv{};
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                retv[i][j] += L[i][k] * R[k][j];
+            }
+        }
+    }
+    return retv;
+}
+
+template <int N>
+Matrix<N> operator*(double c, const Matrix<N>& M)
+{
+    Matrix<N> retv;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            retv[i][j] = M[i][j] * c;
+        }
+    };
+    return retv;
+}
+
+template <int N>
+Matrix<N> operator*( const Matrix<N>& M, double c)
+{
+    Matrix<N> retv;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            retv[i][j] = M[i][j] * c;
+        }
+    };
+    return retv;
+}
+
+template <int N>
+Matrix<N> operator-(const Matrix<N>& L, const Matrix<N>& R)
+{
+    Matrix<N> retv;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            retv[i][j] = L[i][j] - R[i][j];
+        }
+    };
+    return retv;
+}
+
+template <int N>
+Matrix<N> operator+(const Matrix<N>& L, const Matrix<N>& R)
+{
+    Matrix<N> retv;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            retv[i][j] = L[i][j] + R[i][j];
+        }
+    };
+    return retv;
+}
+
+//
 
 template <int N>
 class Rls
 {
-    Matrix<double, N, N> P_old{ { 0.0 } }, I{ { 0.0 } };
-    std::array<double, N> theta_old{ 0.0 }, gamma{ 0.0 }, x{ 0.0 }, temp{ 0.0 };
+    Matrix<N> P_old{}, I{};
+    std::array<double, N> theta_old{}, gamma{}, x{}, tempv{};
     double err=0.0, y=0.0, alpha=0.0;
     double lambda = 0.99;
 
-    double scalar(std::array<double, N> x, std::array<double, N> y);
-    Matrix<double, N, N> outer_prod(std::array<double, N> x, std::array<double, N> y);
-    Matrix<double, N, N> subtract_mat(Matrix<double, N, N> X, Matrix<double, N, N> Y);
-    Matrix<double, N, N> product_mat(Matrix<double, N, N> X, Matrix<double, N, N> Y);
-    Matrix<double, N, N> scalar_mat(double x, Matrix<double, N, N> Y);
+    double dot(std::array<double, N> x, std::array<double, N> y);
+    Matrix<N> outer_prod(std::array<double, N> x, std::array<double, N> y);
 public:
     Rls(double alpha = 100, double lambda = 0.99); // alpha is initial variance for matrix P
     Rls(std::array<double, N> theta_initial,double alpha = 10, double lambda = 1); // alpha is initial variance for matrix P
@@ -26,7 +107,7 @@ public:
 
 
 template<int N>
-inline double Rls<N>::scalar(std::array<double, N> x, std::array<double, N> y)
+inline double Rls<N>::dot(std::array<double, N> x, std::array<double, N> y)
 {
     double retv = 0;
     for (int i = 0; i < N; i++) {
@@ -36,7 +117,7 @@ inline double Rls<N>::scalar(std::array<double, N> x, std::array<double, N> y)
 }
 
 template<int N>
-inline Matrix<double, N, N> Rls<N>::outer_prod(std::array<double, N> x, std::array<double, N> y)
+inline Matrix<N> Rls<N>::outer_prod(std::array<double, N> x, std::array<double, N> y)
 {
     std::array<std::array<double, N>, N> retv;
     for (int i = 0; i < N; i++) {
@@ -45,43 +126,6 @@ inline Matrix<double, N, N> Rls<N>::outer_prod(std::array<double, N> x, std::arr
         }
     };
     return retv;
-}
-
-template<int N>
-inline Matrix<double, N, N> Rls<N>::subtract_mat(Matrix<double, N, N> X, Matrix<double, N, N> Y)
-{
-    Matrix<double, N, N> retv;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            retv[i][j] = X[i][j] - Y[i][j];
-        }
-    };
-    return retv;
-}
-
-template<int N>
-inline Matrix<double, N, N> Rls<N>::product_mat(Matrix<double, N, N> X, Matrix<double, N, N> Y)
-{
-    std::array<std::array<double, N>, N> retv{ {0.0} };
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
-                retv[i][j] += X[i][k] * Y[k][j];
-            }
-        }
-    };
-    return retv;
-}
-
-template<int N>
-inline Matrix<double, N, N> Rls<N>::scalar_mat(double x, Matrix<double, N, N> Y)
-{
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            Y[i][j] *=x;
-        }
-    };
-    return Y;
 }
 
 template<int N>
@@ -103,16 +147,15 @@ template<int N>
 void inline Rls<N>::update(std::array<double, N> x_new, double y_new)
 {
     x = x_new; y = y_new;
-
-    err = y - scalar(x, theta_old);
+    err = y - dot(x, theta_old);
     // new gamma
-    for (int i = 0; i < N; i++) { temp[i] = scalar(P_old[i], x); }
-    double c = 1.0 / (scalar(x, temp) + lambda);
-    for (int i = 0; i < N; i++) { gamma[i] = c * temp[i]; }
+    for (int i = 0; i < N; i++) { tempv[i] = dot(P_old[i], x); }
+    double c = 1.0 / (dot(x, tempv) + lambda);
+    gamma = c * tempv;
     // new theta
     for (int i = 0; i < N; i++) { theta_old[i] += err * gamma[i]; }
     // new P
-    P_old= scalar_mat(1.0 / lambda, product_mat(subtract_mat(I,outer_prod(gamma,x)), P_old));    
+    P_old = (1.0 / lambda) * (I-outer_prod(gamma,x)) * P_old;    
 }
 
 template<int N>
